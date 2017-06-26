96826c2c11badc8ef4a3eabb4e6cbcbef1ebef31
Kavelaars <indykavelaars@gmail.com>
2017-06-26 21:16:12 +0200

    Summary till 2.6

--- indy.py
+++ indy.py
@@ -1,17 +1,29 @@
-print 'Samenvatting - Hoofdstuk 1, 2 en 4.12'
-print 'Basic instructions programming languages'
-print '- Input, get data example; keyboard'
-print '- Output, display data example; to screen'
-print '- Math, basic mathematical operations'
-print '- Conditional excution, check conditions and excute appropriately'
-print '- Repetition, Perform action reapetdly with some variation'
-print 'Two kinds of programs processes high-level into low level languages'
-print '1. Interpreters, sourcecode -> output. processes al little at a time'
-print '2. Compilers, translates sourcecode completely before running to objectcode/executable. Once compiled it can run without translation'
-print 'Different Errors'
-print '1. Semantic errors, program will run but does not do what you want it to do'
-print '2. Runtime errors, program will not run, are also called exceptions, rare in simple programs'
-print '3. Syntax errors, code is not correctly formatted'
-print 'Programming languages are formal languages that have been designed to express computations'
-print 'Exercise 1.4: 1. l
-
+print "Samenvatting - Hoofdstuk 1, 2 en 4.12"
+print "Basic instructions programming languages"
+print "- Input, get data example; keyboard"
+print "- Output, display data example; to screen"
+print "- Math, basic mathematical operations"
+print "- Conditional excution, check conditions and excute appropriately"
+print "- Repetition, Perform action reapetdly with some variation"
+print "Two kinds of programs processes high-level into low level languages"
+print "1. Interpreters, sourcecode -> output. processes al little at a time"
+print "2. Compilers, translates sourcecode completely before running to objectcode/executable. Once compiled it can run without translation"
+print "Different Errors"
+print "1. Semantic errors, program will run but does not do what you want it to do"
+print "2. Runtime errors, program will not run, are also called exceptions, rare in simple programs"
+print "3. Syntax errors, code is not correctly formatted"
+print "Programming languages are formal languages that have been designed to express computations"
+print "Exercise 1.4: 1. l"
+print "Sol1.4_1 - This is @ well structured sentence, but wi|h unrecognizable characters in it."
+print "Sol1.4_2 - This is not well a structured. sentence"
+print "Parsing, figuring out what a sentence means"
+print type("Hello, World")
+print type(0)
+print type(3.2)
+print 1,000,000
+message = "The cool kid"
+print message
+print type (message)
+print "Keywords: and def exec if not return assert del finally import or try break elif for in pass while class else from is print yield continue except global lambda raise"
+print "sol 2.5 - put the keyword print before the values"
+print "till 2.6"
