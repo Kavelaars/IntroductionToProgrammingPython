7fa45dceb0c828cc32a5f84d30962eb630fe669b
Kavelaars <indykavelaars@gmail.com>
2017-06-28 20:37:11 +0200

    Summary 1,2 and 4.12
    Samenvatting

--- indy.py
+++ indy.py
@@ -1,3 +1,8 @@
+# IK
+# indy.py
+# MIT A Gentle Introduction to Programming Using Python
+# https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-189-a-gentle-introduction-to-programming-using-python-january-iap-2011/
+# 28 June, 2017 
 print "Samenvatting - Hoofdstuk 1, 2 en 4.12"
 print "Basic instructions programming languages"
 print "- Input, get data example; keyboard"
@@ -21,9 +26,23 @@ print type("Hello, World")
 print type(0)
 print type(3.2)
 print 1,000,000
-message = "The cool kid"
+who = "The cool kid"
 print message
 print type (message)
 print "Keywords: and def exec if not return assert del finally import or try break elif for in pass while class else from is print yield continue except global lambda raise"
 print "sol 2.5 - put the keyword print before the values"
-print "till 2.6"
+print "Operators special symbols, operands are the values"
+print "Minute variable assigned to 59(int)"
+minute = 59
+print minute/60 #Int devision
+print minute//60 #Double slash means I know this is a integer devision, and it is correct!
+print minute*100/60 #You can also put comments at the at of a line
+print "not a floating point devision"
+what = " doing some Python"
+print who + what + " concatination and having, " + "Fun! "*3
+# This is a comment and is not visible when the program is executed
+print "Chapter 4.12 keyboard input"
+name = raw_input ("What ... is your name? ")
+print name
+prompt = "How fast is a cheata? km/h \n" # \n means newLine, input needs to be int
+speed = input(prompt)
